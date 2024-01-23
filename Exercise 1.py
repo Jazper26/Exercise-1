b9a5c3777629d8edc38d0492dc459cdc9b5a6459
Jazper26 <jazperhaapin89Gmail.com>
2024-01-23 22:18:00 +0800

    Fixing the comment on if else

--- Exercise 1.py
+++ Exercise 1.py
@@ -7,6 +7,6 @@ product = num1*num2
 sum= num1+num2
     
 if product <= 1000:
-    print ("The result is: ", product) # Printing the product if >= 1000
+    print ("The result is: ", product) # Printing the product if <= 1000
 else:
     print ("The result is: ", sum) # Else Statement
